196cee1fe27a5f4a74e2e12b71c707d5405c1349
MatiasCollaguazo <mcollagu@espol.edu.ec>
2024-12-09 14:14:54 -0500

    Guia17-1

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,34 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonCard, IonCardHeader, IonCardTitle, IonCardContent,
+     IonSelect, IonSelectOption, IonTextarea,IonButton,
+     IonList, IonItem, IonLabel,IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+  
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [ ReactiveFormsModule, IonCard, IonCardHeader, IonCardTitle, IonCardContent,
+         IonSelect, IonSelectOption, IonTextarea,IonButton,
+         IonList, IonItem, IonLabel,IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
 })
 export class Tab2Page {
+   myForm: FormGroup = new FormGroup({
+     score: new FormControl("", Validators.required),
+     opinion: new FormControl("", Validators.required)
+   });
 
-  constructor() {}
+   onSubmit() {
+       console.log(this.myForm.value);
+       alert(this.myForm.controls["score"].value)
+       this.myForm.reset()
+   }
+   
+  constructor() {
+
+  }
 
 }
